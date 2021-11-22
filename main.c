fe4f6d853ae24c46bf453a8c43fa8097369064f2
chaimae mita <chaimae.mita@gmail.com>
2021-11-22 16:48:08 +0100

    projet

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void CompteBancaire ();
-
-struct compte
+struct compteUSER
 {
     char CIN[10];
     char NOM[20];
     char PRENOM [20];
     float montant;
-};
+}User;
+
+
+void CompteBancaire ()
+{
+    int check = 0;
+    struct  User;
+    printf("NOM: \n\t");
+    scanf("%s",User.NOM);
+    printf("PRENOM: \n\t");
+    scanf("%s",User.PRENOM);
+    printf("CIN: \n\t");
+    scanf("%d",User.CIN);
+
+}
 
 
 int main()
 {
     int choix;
     MENU:
-        do
-        {
+
 
     printf("Menu principale \n");
     printf("1-Introduire un compte bancaire: \n2-Introduire plusieurs comptes bancaire: \n3-Operations: \n4-Afichage: \n5-Fidalisation: \n6-Quitter l'application.\n");
@@ -39,15 +49,9 @@ int main()
             goto MENU;
         }
         break;
-    case 2:
-        printf("Introduire plusieurs comptes bancaires:\n");
-        printf("veuillez remplir la fiche suivante: \n");
-
-    default:
-        break;
-    }while(choix <1 || choix >6);
 
     return 0;
+    }
 }
 
 
