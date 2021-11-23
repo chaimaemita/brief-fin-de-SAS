b6cbf5a1c23aba8312b1056c2b5d747f2703dc5c
chaimae mita <chaimae.mita@gmail.com>
2021-11-23 21:15:29 +0100

    projet

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct compteUSER
 {
@@ -12,11 +13,69 @@ struct compteUSER
 struct compteUSER db[50];
 int i = 0;
 
+void MENU ()
+{
+   printf("\n1- Creer un  compte \n");
+   printf("2- Creer plusieurs comptes \n");
+   printf("3- Afficher mon compte/solde \n");
+   printf("4- Operations \n");
+   printf("6- Affichage \n");
+   printf("7- Quitter l'application \n\n");
+}
 
+int main()
+{
+    int choix,c,l=1;
+    printf("*****************YOUCODE BANC******************\n");
+    MENU ();
+     while (1){
+    scanf("%d",&choix);
+    switch (choix)
+    {
+    case 1:
+        printf("creer un compte : \n");
+        printf("veuillez remplir la fiche suivante: \n");
+        CompteBancaire ();
+        printf("Pour revenir au menu principale, entrez 0. \n");
+        scanf("%d",&l);
+        if (l == 0)
+        {
+            MENU ();
+        }
+        break;
+
+    case 2:
+       printf("creer plusieurs comptes bancaire: \n");
+       otherAcc ();
+       printf("Pour revenir au menu principale, entrez 0. \n");
+        scanf("%d",&l);
+        if (l == 0)
+        {
+            MENU ();
+        }
+       break;
+    case 3:
+        printf("Operations: \n");
+        printf("1- Depot.\n");
+        printf("2- Retrait. \n");
+        scanf("%d",&c);
+        if(c == 1)
+        {
+           depotMoney ();
+        }
+        else
+        {
+            retraitMoney ();
+        } break;
+
+    return 0;
+}
+}
+}
 void CompteBancaire ()
 {
     struct compteUSER User;
-
+    printf("*****VOS INFORMATIONS*****\n");
     printf("NOM: \n\t");
     scanf("%s",User.NOM);
     printf("PRENOM: \n\t");
@@ -25,84 +84,21 @@ void CompteBancaire ()
     scanf("%s",User.CIN);
     printf("montant: \n\t");
     scanf("%f",&User.montant);
+    printf("\n\t VOTRE COMPTE A ETE CREER AVEC SUCCES.\n");
     db[i++] = User;
 }
-
-
-
-int main()
+void otherAcc ()
 {
-    int choix,c;
-    MENU:
-
-    printf("Menu principale \n");
-    printf("1-Introduire un compte bancaire: \n2-Introduire plusieurs comptes bancaire: \n3-Operations: \n4-Afichage: \n5-Fidalisation: \n6-Quitter l'application.\n");
-    scanf("%d",&choix);
-
-    switch (choix)
-    {
-    case 1:
-        printf("Introduire un compte bancaire: \n");
-        int l=1;
-        printf("veuillez remplir la fiche suivante: \n");
-        CompteBancaire ();
-        printf("\n Votre compte bancaire a ete cree avec succes. \n");
-        scanf(" ");
-        printf("Pour revenir au menu principale, entrez 0. \n");
-        scanf("%d",&l);
-        if (l == 0)
-        {
-            goto MENU;
-        }
-        break;
-
-    case 2:
-       printf("Troduire plusieurs comptes bancaire: \n");
-       int n;
+       int n,i=1;
+       printf("\nVeuillez entrer le nombre du nouveax comptes que vous voulez creer:");
+       scanf("%d",&n);
        printf("Veuillez remplir la fiche suivante: \n");
-       CompteBancaire ();
-       printf("le compte bancaire a ete cree avec succes. \n");
        do
        {
-           printf(" Creer plusieurs comptes. \n");
-           printf("combien de compte volez vous cree ?");                                                                               
-           scanf("%d",&n);
            CompteBancaire ();
-       }while (choix<0 || choix>n);
-       break;
-
-    case 3:
-        printf("Operations: \n");
-        printf("1-Depot.\n");
-        printf("2-retrait. \n");
-        scanf("%d",&c);
-        if(c == 1)
-        {
-           depotMoney (); 
-        }
-        else 
-        {
-            retraitMoney ();
-        } break;
-
-    case 4:
-
-
-
-
-    
-
-
-
-
-    
-
-
-
-
-    return 0;
+           i++;
+       }while (i<=n);
 }
-
 void depotMoney ()
 {
     int som;
@@ -121,7 +117,6 @@ void depotMoney ()
         }
         b++;
     }
-
 }
 void retraitMoney ()
 {
@@ -136,13 +131,26 @@ void retraitMoney ()
     //find user with cin
     while(b<50)
     {
-        if(strcmp(db[b].CIN,cin) == 0)){
+        if(strcmp(db[b].CIN,cin) == 0)
+        {
             db[b].montant -= som;
             break;
         }
         b++;
     }
 }
+/*void OrdreAscendant ()
+{
+    int i,j,tmp;
+    printf("Affichage des comptes par ordre Ascendant:\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
 
+        }
+    }
 
+}
+*/
 
